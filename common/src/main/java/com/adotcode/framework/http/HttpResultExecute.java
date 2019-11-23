cd2b310527a305c8a1c81e32789588b717e9255a
risfeng <risfeng@126.com>
2019-11-24 00:04:04 +0800

    init

---
+++ common/src/main/java/com/adotcode/framework/http/HttpResultExecute.java
@@ -0,0 +1,18 @@
+package com.adotcode.framework.http;
+
+/**
+ * Http请求统一返回执行接口
+ *
+ * @author risfeng
+ * @date 2019/11/22
+ */
+public interface HttpResultExecute<T> {
+
+  /**
+   * 返回结果执行
+   *
+   * @return T
+   * @throws Exception 异常
+   */
+  public T execute() throws Exception;
+}
